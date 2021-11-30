ee6d1cbcaa8e2029cb0367026fa1307242f1426d
kgthompson1111 <kgthompson@null.net>
2021-11-29 22:39:34 -0700

    add form

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // initialize library variable
 let myLibrary = [];
 
+    //hide the book form
+    bookFormBG.setAttribute('style', 'display:none');
+
+    //show and hide form functions for book form
+    function showForm() {
+        bookFormBG.setAttribute('style', 'display: yes');
+        addBook.removeEventListener('click', showForm);
+        addBook.addEventListener('click', hideForm);
+        addBook.innerText = "hide form";
+    }
+
+    function hideForm() {
+        bookFormBG.setAttribute('style', 'display: none');
+        addBook.removeEventListener('click', hideForm);
+        addBook.addEventListener('click', showForm);
+        addBook.innerText = "show form";
+    }
 
     // create new BOOK object that contains a title, author, #pages, status
 
@@ -27,61 +44,66 @@ let myLibrary = [];
     // displayLibrary function - loops through the myLibrary array and adds a card for each book
 
     function displayLibrary() {
+        container.innerHTML = ``;
         for(i = 0; i < myLibrary.length; i++) {
-            const bookCard = document.createElement('div');
-            bookCard.setAttribute('id', [i]);
-            bookCard.classList.add('book');
-            myLibrary[i].read ? bookCard.classList.add('read') : null;
-            bookCard.innerHTML = 
-                `${myLibrary[i].author} - ${myLibrary[i].title}<br>
-                 ${myLibrary[i].pages} pages.<br>
-                `;
+            if(myLibrary[i] != undefined) {
+                const bookCard = document.createElement('div');
+                bookCard.setAttribute('id', [i]);
+                bookCard.classList.add('book');
+                myLibrary[i].read ? bookCard.classList.add('read') : null;
+                bookCard.innerHTML = 
+                    `${myLibrary[i].author} - ${myLibrary[i].title}<br>
+                    ${myLibrary[i].pages} pages.<br>
+                    `;
 
-            // create and append a "read" button that changes the read status
-            const readButton = document.createElement('button');
-            readButton.setAttribute('class', `readButton`);
-            if(myLibrary[i].read) {
-                readButton.innerText = 'READ'
-                readButton.classList.add('read');
-            }  else if(!myLibrary[i].read) {
-                readButton.innerText = 'UNREAD';
-            }
-
-            readButton.addEventListener('click', (e) => {
-                const i = parseInt(e.target.parentNode.id);
-                if(myLibrary[i].read == true) {
-                // if read and button clicked - set read to false and update
-                myLibrary[i].read = false;
-                readButton.innerText = 'UNREAD';
-                readButton.classList.remove('read');
-                e.target.parentNode.classList.remove('read');
-                //if unread and button clicked - set read to true and update
-                } else if(myLibrary[i].read == false) {
-                myLibrary[i].read = true;
-                readButton.innerText = 'READ';
-                readButton.classList.add('read');
-                e.target.parentNode.classList.add('read');
+                // create and append a "read" button that changes the read status
+                const readButton = document.createElement('button');
+                readButton.setAttribute('class', `readButton`);
+                if(myLibrary[i].read) {
+                    readButton.innerText = 'READ'
+                    readButton.classList.add('read');
+                }  else if(!myLibrary[i].read) {
+                    readButton.innerText = 'UNREAD';
                 }
-            });
 
-            const deleteButton = document.createElement('button');
-            deleteButton.setAttribute('class', `deleteButton`);
-            deleteButton.innerText = 'X';
+                readButton.addEventListener('click', (e) => {
+                    const i = parseInt(e.target.parentNode.id);
+                    if(myLibrary[i].read == true) {
+                    // if read and button clicked - set read to false and update
+                    myLibrary[i].read = false;
+                    readButton.innerText = 'UNREAD';
+                    readButton.classList.remove('read');
+                    e.target.parentNode.classList.remove('read');
+                    //if unread and button clicked - set read to true and update
+                    } else if(myLibrary[i].read == false) {
+                    myLibrary[i].read = true;
+                    readButton.innerText = 'READ';
+                    readButton.classList.add('read');
+                    e.target.parentNode.classList.add('read');
+                    }
+                });
 
-            bookCard.appendChild(deleteButton);
-            bookCard.appendChild(readButton);
-            container.appendChild(bookCard);
+                const deleteButton = document.createElement('button');
+                deleteButton.setAttribute('class', `deleteButton`);
+                deleteButton.innerText = 'X';
 
-    }
+                deleteButton.addEventListener('click', (e) => {
+                    const i = parseInt(e.target.parentNode.id);
+                    const currentNode = document.getElementById(`${i}`);
+                    e.target.parentNode.parentNode.removeChild(currentNode);
+                    delete myLibrary[i];
+                    console.log(myLibrary);
+                    //renumber the array
+                });
 
-            // const readButton = document.querySelectorAll('.readButton');
-            // readButton.forEach((button) => {
-            //     button.addEventListener('click', (e) => {
-            //         console.log(e.target);
-            //         readText.innerText = "boop";
-            //     });
-            // });
-        
+                bookCard.appendChild(deleteButton);
+                bookCard.appendChild(readButton);
+                container.appendChild(bookCard);
+            }
+        }
     }
 
         displayLibrary();
+
+        addBook.addEventListener('click', showForm);
+    
