1aa914e5b1e5d0c21da0c8e0df4e78c45f531a55
kgthompson1111 <kgthompson@null.net>
2021-11-30 20:11:42 -0700

    added form functionality

--- script.js
+++ script.js
@@ -46,14 +46,13 @@ let myLibrary = [];
     function displayLibrary() {
         container.innerHTML = ``;
         for(i = 0; i < myLibrary.length; i++) {
-            if(myLibrary[i] != undefined) {
                 const bookCard = document.createElement('div');
                 bookCard.setAttribute('id', [i]);
                 bookCard.classList.add('book');
                 myLibrary[i].read ? bookCard.classList.add('read') : null;
                 bookCard.innerHTML = 
                     `${myLibrary[i].author} - ${myLibrary[i].title}<br>
-                    ${myLibrary[i].pages} pages.<br>
+                    ${myLibrary[i].pages} pages<br>
                     `;
 
                 // create and append a "read" button that changes the read status
@@ -83,6 +82,8 @@ let myLibrary = [];
                     }
                 });
 
+                // create and append a delete button
+
                 const deleteButton = document.createElement('button');
                 deleteButton.setAttribute('class', `deleteButton`);
                 deleteButton.innerText = 'X';
@@ -92,18 +93,108 @@ let myLibrary = [];
                     const currentNode = document.getElementById(`${i}`);
                     e.target.parentNode.parentNode.removeChild(currentNode);
                     delete myLibrary[i];
-                    console.log(myLibrary);
                     //renumber the array
                 });
 
                 bookCard.appendChild(deleteButton);
                 bookCard.appendChild(readButton);
                 container.appendChild(bookCard);
-            }
         }
     }
 
-        displayLibrary();
+displayLibrary();
 
-        addBook.addEventListener('click', showForm);
+addBook.addEventListener('click', showForm);
     
+submit.addEventListener('click', () => {
+
+    const pagesInt = parseInt(bookForm.elements['pages'].value);
+
+    // throw an error for erroneous user data
+
+    if(bookForm.elements['author'].value == "") {
+        alert("Author must be entered");
+        return;
+    } else if(bookForm.elements['title'].value == "") {
+        alert("Title must be entered");
+        return;
+    } else if(isNaN(pagesInt)) {
+        alert("Book length must be numerical");
+        bookForm.elements['pages'].value = "";
+        return;
+    } else if(pagesInt <= 0) {
+        alert("Book length must be greater than 0 pages");
+        bookForm.elements['pages'].value = "";
+        return;
+
+    } 
+
+    i = myLibrary.length; // new book goes to end of array
+
+    const bookCard = document.createElement('div');
+                bookCard.setAttribute('id', [i]);
+                bookCard.classList.add('book');
+
+    // create new book in the array
+    
+    myLibrary[i] = new Book(
+        `${bookForm.elements['title'].value}`,
+        `${bookForm.elements['author'].value}`,
+        parseInt(bookForm.elements['pages'].value),
+        bookForm.elements['hasRead'].checked ? true : false);
+
+        console.log(typeof(myLibrary[i].read));
+        console.log(myLibrary[i]);
+    // display the book on the page
+
+    myLibrary[i].read ? bookCard.classList.add('read') : null;
+    bookCard.innerHTML = 
+        `${myLibrary[i].author} - ${myLibrary[i].title}<br>
+        ${myLibrary[i].pages} pages<br>
+        `;
+    
+    // create and append a "read" button that changes the read status
+    const readButton = document.createElement('button');
+    readButton.setAttribute('class', `readButton`);
+    if(myLibrary[i].read) {
+        readButton.innerText = 'READ'
+        readButton.classList.add('read');
+    }  else if(!myLibrary[i].read) {
+        readButton.innerText = 'UNREAD';
+    }
+
+    readButton.addEventListener('click', (e) => {
+        const i = parseInt(e.target.parentNode.id);
+        if(myLibrary[i].read == true) {
+        // if read and button clicked - set read to false and update
+        myLibrary[i].read = false;
+        readButton.innerText = 'UNREAD';
+        readButton.classList.remove('read');
+        e.target.parentNode.classList.remove('read');
+        //if unread and button clicked - set read to true and update
+        } else if(myLibrary[i].read == false) {
+        myLibrary[i].read = true;
+        readButton.innerText = 'READ';
+        readButton.classList.add('read');
+        e.target.parentNode.classList.add('read');
+        }
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', `deleteButton`);
+    deleteButton.innerText = 'X';
+
+    deleteButton.addEventListener('click', (e) => {
+        const i = parseInt(e.target.parentNode.id);
+        const currentNode = document.getElementById(`${i}`);
+        e.target.parentNode.parentNode.removeChild(currentNode);
+        delete myLibrary[i];
+        //renumber the array
+    });
+
+    // append elements
+
+    bookCard.appendChild(deleteButton);
+    bookCard.appendChild(readButton);
+    container.appendChild(bookCard);
+});
